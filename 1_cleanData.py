e6fc5cdc6c641d87a47e73d2f331ea09931b8d5d
jasminejoy01 <55416635+jasminejoy01@users.noreply.github.com>
2021-10-25 13:48:43 -0400

    Add files via upload

---
+++ 1_cleanData.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 10:23:02 2021
+
+@author: jasmi
+"""
+
+import pandas as pd
+import re
+
+
+''' Import'''
+df = pd.read_csv('Suicide_Detection.csv') 
+#print(df.shape)
+#print(df.head())
+
+''' Functions'''
+def a(text):
+    chars = "\`*_{}[]()>#+-!$';?<>|:~@%^ðŸ™ðŸ˜ŽÄ°â€€™"
+    for c in chars:
+        text = text.replace(c, " ")
+    text = text.replace('"', " ")
+    text = text.replace(".", " ")
+    text = text.replace(",", " ")
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
+    text = text.replace("'", " ")
+    text = text.replace("filler", "'")
+    text = text.replace("  ", " ")
+    text = text.lower()
+    return text
+
+'''Processing'''
+notes = df['text']
+classfication = df['class']
+notesCleaned = pd.Series( [], dtype=str)
+
+#for i in range(0, len(notes)-1):
+for i in range(0, len(notes)):
+    each = (a(notes[i]))
+    if i%10000 == 0:
+        #print('At ', i, 'th record.')
+        print("At record = %s" %(i))
+    notesCleaned[i] = each
+    #print(each)
+
+df['notesCleaned'] = notesCleaned
+
+'''Export'''
+df.to_csv('c_Suicide_Detection.csv', index=False)
