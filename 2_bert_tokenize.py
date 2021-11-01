680917838dd4c9f487984dad736eaf58e779febb
jasminejoy01 <55416635+jasminejoy01@users.noreply.github.com>
2021-11-01 11:57:08 -0400

    Add files via upload

---
+++ 2_bert_tokenize.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 11:53:56 2021
+
+@author: jasmi
+"""
+
+import numpy as np
+import pandas as pd
+import torch
+import transformers as ppb # pytorch transformers
+
+#tokenizer.encode("a visually stunning rumination on love", add_special_tokens=True)
+
+#Import Data
+train_df = pd.read_csv('C:/Users/jasmi/Desktop/ML data/bert_test_data.csv') 
+train_df.head()
+
+index = train_df['Unnamed: 0']
+notes = train_df['text']
+classfication = train_df['class']
+notesCleaned = train_df['notesCleaned']
+
+#Importing pre-trained BERT model
+model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
+
+#Loading pretrained model/tokenizer
+tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+model = model_class.from_pretrained(pretrained_weights)
+
+#Output
+tokenized = train_df['notesCleaned'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
