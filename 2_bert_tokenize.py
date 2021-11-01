d3d76858b19ee1ab20ef15f442dae7cf6f7f39b8
jasminejoy01 <55416635+jasminejoy01@users.noreply.github.com>
2021-11-01 18:28:18 -0400

    Update 2_bert_tokenize.py

--- 2_bert_tokenize.py
+++ 2_bert_tokenize.py
@@ -9,22 +9,46 @@ import numpy as np
 import pandas as pd
 import torch
 import transformers as ppb # pytorch transformers
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 
-#Import Data
-train_df = pd.read_csv('C:/ML data/bert_test_data.csv') 
-train_df.head()
+'''Import Data'''
+name = 'sample_10'
+df = pd.read_csv('C:/Users/jasmi/Desktop/ML data/'+name+'.csv') 
+df.head()
 
-index = train_df['Unnamed: 0']
-notes = train_df['text']
-classfication = train_df['class']
-notesCleaned = train_df['notesCleaned']
+index = df['Unnamed: 0']
+notes = df['text']
+classfication = df['class']
+notesCleaned = df['notesCleaned']
+df.loc[df['class'] == 'suicide', 'classification_int'] = 1
+df.loc[df['class'] == 'non-suicide', 'classification_int'] = 0
 
-#Importing pre-trained BERT model
+
+'''Importing pre-trained BERT model'''
 model_class, tokenizer_class, pretrained_weights = (ppb.BertModel, ppb.BertTokenizer, 'bert-base-uncased')
 
-#Loading pretrained model/tokenizer
-tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
-model = model_class.from_pretrained(pretrained_weights)
+'''Loading pretrained model/tokenizer'''
+tokenizer = tokenizer_class.from_pretrained(pretrained_weights, padding=True, truncation=True, return_tensors="pt")
+#model = model_class.from_pretrained(pretrained_weights)
 
-#Output
-tokenized = train_df['notesCleaned'].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+'''Output'''
+tokenized = notesCleaned.apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+tokens = pd.Series( [], dtype=str)
+
+max_value = 0
+
+print("\n",(df['notesCleaned']).shape, tokenized.shape)
+i = 0
+while i < len(df['notesCleaned']):
+    #tokens[i] = np.pad(tokenized[i], (0, 512-len(tokenized[i])), 'constant', constant_values=(0))
+    tokens[i] = tokenized[i]
+    if len(tokenized[i]) >= max_value:
+        max_value = len(tokenized[i])
+    print(classfication[i], tokenized.shape, len(tokenized[i]), max_value)
+    i = i + 1
+
+'''Export'''
+df['max_count'] = max_value
+df['tokens'] = tokens
+df.to_csv(name+'_tokens.csv', index=False)
