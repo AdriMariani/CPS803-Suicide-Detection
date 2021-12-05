dd1b77333c684203f285e5e9e02532453b8cd558
Adriano Mariani <adriano.mariani@ryerson.ca>
2021-12-04 22:59:17 -0500

    use CountVectorizer for nb

--- classifiers/naive_bayes.py
+++ classifiers/naive_bayes.py
@@ -1,64 +1,61 @@
 import pandas as pd
 import numpy as np
-import utils
+import sys
+sys.path.append('..')
+import utils.utils as utils
+import utils.evaluation as evaluation
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import BernoulliNB
 from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 
-'''Import Training Data'''
-train_name = 'Suicide_Detection_tokens1'
-df = pd.read_csv(train_name+'.csv') 
-train_df = df.loc[df['limit'] == 0] #filter records where tokens <512
-#train_df.head()
+path = '../datasets/c_Suicide_Detection.csv'
+df = pd.read_csv(path, header=0)
 
-'''Import Test Data'''
-test_name = 'reddit_depression_suicidewatch_tokens1'
-test_df = pd.read_csv(test_name+'.csv') 
-#train_df.head()
+sentences = df['notesCleaned'].values
 
-'''Training Dataframes'''
-index = train_df['id']
-notes = train_df['text']
-classfication = train_df['class']
-notesCleaned = train_df['notesCleaned']
-classfication_int = train_df['classification_int']
-tokens = train_df['tokens']
-max_count = train_df['max_count']
+ex = sentences.shape[0]
+y = np.zeros((ex))
+y[df['class'] == 'suicide'] = 1
 
-test_index = test_df['id']
-test_notes = test_df['text']
-test_classfication = test_df['class']
-test_notesCleaned = test_df['notesCleaned']
-test_classfication_int = test_df['classification_int']
-test_tokens = test_df['tokens']
-test_max_count = test_df['max_count']
+sentences = sentences[:15000]
+y = y[:15000]
 
-''' Additional Columns '''
-#finalpredictions = pd.Series( [], dtype=str)
+sentences_train, sentences_test, y_train, y_test = train_test_split(
+    sentences, y, test_size=0.25, random_state=1000)
 
-'''Constants'''
-if max_count[0] > 512 or test_max_count[0]:
-    padding = 512
-else:
-    padding = max(max_count[0], test_max_count[0])
-
-train_matrix = utils.create_matrix(tokens, padding)
-test_matrix = utils.create_matrix(test_tokens, padding)
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences_train)
+train_matrix = vectorizer.transform(sentences_train)
+test_matrix  = vectorizer.transform(sentences_test)
 
 '''Bernoulli Naive Bayes Classification'''
 bnb = BernoulliNB(alpha=1.0, binarize=0.0, fit_prior=True, class_prior=None)
-bnb = bnb.fit(train_matrix, classfication_int)
+bnb = bnb.fit(train_matrix, y_train)
 predictionsNB = bnb.predict(test_matrix)
-scoreNB = metrics.accuracy_score(test_classfication_int, predictionsNB)
+scoreNB = metrics.accuracy_score(y_test, predictionsNB)
+bnb_confusion_matrix = evaluation.confusion_matrix(y_test, predictionsNB)
 #print(predictionsNB)
 #test_df['predictionsNB'] = predictionsNB
 print("Bernoulli Naive Bayes Prediction Score:", round(scoreNB*100, 2), "%")
+print("Confusion Matrix:", bnb_confusion_matrix)
+print("Precision:", evaluation.calc_precision(bnb_confusion_matrix))
+print("Sensitivity/Positive Recall:", evaluation.calc_sensitivity(bnb_confusion_matrix))
+print("Specificity/Negative Recall:", evaluation.calc_specificity(bnb_confusion_matrix))
+print("F1 Score:", evaluation.calc_f1_score(bnb_confusion_matrix))
 
 '''Gaussian Naive Bayes Classification'''
 gnb = GaussianNB(var_smoothing=1e-5)
-gnb = gnb.fit(train_matrix, classfication_int)
-predictionsGNB = gnb.predict(test_matrix)
-scoreGNB = metrics.accuracy_score(test_classfication_int, predictionsGNB)
+gnb = gnb.fit(train_matrix.todense(), y_train)
+predictionsGNB = gnb.predict(test_matrix.todense())
+scoreGNB = metrics.accuracy_score(y_test, predictionsGNB)
+gnb_confusion_matrix = evaluation.confusion_matrix(y_test, predictionsGNB)
 #print(predictionsGNB)
 #test_df['predictionsGNB'] = predictionsGNB
 print("Gaussian Naive Bayes Prediction Score:", round(scoreGNB*100, 2), "%")
+print("Confusion Matrix:", gnb_confusion_matrix)
+print("Precision:", evaluation.calc_precision(gnb_confusion_matrix))
+print("Sensitivity/Positive Recall:", evaluation.calc_sensitivity(gnb_confusion_matrix))
+print("Specificity/Negative Recall:", evaluation.calc_specificity(gnb_confusion_matrix))
+print("F1 Score:", evaluation.calc_f1_score(gnb_confusion_matrix))
